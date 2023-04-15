f734f51b7e4cd3a1a8aca31bdf749fbe1fcf939c
triskil <trisskil@gmail.com>
2023-04-15 11:26:50 -0400

    Updated working plane and sphere render.

--- src/main/kotlin/com/triskil/orbitvisualizer/OrbitVisualizerApp.kt
+++ src/main/kotlin/com/triskil/orbitvisualizer/OrbitVisualizerApp.kt
@@ -2,20 +2,18 @@ package com.triskil.orbitvisualizer
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.*
-import com.badlogic.gdx.graphics.g3d.Environment
-import com.badlogic.gdx.graphics.g3d.Material
-import com.badlogic.gdx.graphics.g3d.Model
-import com.badlogic.gdx.graphics.g3d.ModelBatch
-import com.badlogic.gdx.graphics.g3d.ModelInstance
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.PerspectiveCamera
+import com.badlogic.gdx.graphics.VertexAttributes.Usage
+import com.badlogic.gdx.graphics.g3d.*
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder
+import com.badlogic.gdx.graphics.g3d.utils.shapebuilders.SphereShapeBuilder
 import com.badlogic.gdx.math.Matrix4
 import com.badlogic.gdx.math.Vector3
 import kotlin.random.Random
-import com.badlogic.gdx.graphics.VertexAttributes.Usage
-import com.badlogic.gdx.graphics.g3d.utils.CameraInputController
-
 
 class OrbitVisualizerApp : ApplicationAdapter() {
     private lateinit var camera: PerspectiveCamera
@@ -44,11 +42,6 @@ class OrbitVisualizerApp : ApplicationAdapter() {
 
         val planeModel = createPlaneModel(5f, 32)
         planeInstance = ModelInstance(planeModel)
-        val rotationRange = 45f
-        val randomRotation = Matrix4().rotate(Vector3.X, Random.nextFloat() * rotationRange - rotationRange / 2)
-            .rotate(Vector3.Y, Random.nextFloat() * rotationRange - rotationRange / 2)
-            .rotate(Vector3.Z, Random.nextFloat() * rotationRange - rotationRange / 2)
-        planeInstance.transform.set(randomRotation)
 
         objectInstances = mutableListOf()
         val planeRadius = 5f
@@ -59,12 +52,27 @@ class OrbitVisualizerApp : ApplicationAdapter() {
             val randomAngle = Random.nextDouble() * 2 * Math.PI
             val randomDistance = Random.nextDouble() * planeRadius
             val x = (randomDistance * Math.cos(randomAngle)).toFloat()
-            val y = (randomDistance * Math.sin(randomAngle)).toFloat()
-            val z = 0f
+            val y = 0f
+            val z = (randomDistance * Math.sin(randomAngle)).toFloat()
 
             objectInstance.transform.setTranslation(x, y, z)
-            objectInstance.transform.mul(randomRotation)
             objectInstances.add(objectInstance)
+        }
+
+        val rotationRange = 45f
+        val randomRotation = Matrix4().rotate(Vector3.X, Random.nextFloat() * rotationRange - rotationRange / 2)
+            .rotate(Vector3.Y, Random.nextFloat() * rotationRange - rotationRange / 2)
+            .rotate(Vector3.Z, Random.nextFloat() * rotationRange - rotationRange / 2)
+
+        // Apply the random rotation to the plane
+        planeInstance.transform.set(randomRotation)
+
+        // Apply the random rotation to the boxes and then translate them to the new position
+        objectInstances.forEach {
+            val boxPosition = Vector3(it.transform.getTranslation(Vector3()))
+            it.transform.mul(randomRotation)
+            val rotatedBoxPosition = boxPosition.mul(randomRotation)
+            it.transform.setTranslation(rotatedBoxPosition)
         }
 
         camController = CameraInputController(camera)
@@ -80,47 +88,6 @@ class OrbitVisualizerApp : ApplicationAdapter() {
         objectInstances.forEach { modelBatch.render(it, environment) }
         modelBatch.end()
     }
-
-
-//
-//    override fun create() {
-//        camera = PerspectiveCamera(67f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
-//        camera.position.set(10f, 10f, 10f)
-//        camera.lookAt(0f, 0f, 0f)
-//        camera.near = 1f
-//        camera.far = 1000f
-//        camera.update()
-//
-//        modelBatch = ModelBatch()
-//
-//        environment = Environment()
-//        environment.set(ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f))
-//
-//        val planeModel = createPlaneModel(5f, 32)
-//        planeInstance = ModelInstance(planeModel)
-//        val rotationRange = 45f
-//        val randomRotation = Matrix4().rotate(Vector3.X, Random.nextFloat() * rotationRange - rotationRange / 2)
-//            .rotate(Vector3.Y, Random.nextFloat() * rotationRange - rotationRange / 2)
-//            .rotate(Vector3.Z, Random.nextFloat() * rotationRange - rotationRange / 2)
-//        planeInstance.transform.set(randomRotation)
-//
-//        objectInstances = mutableListOf()
-//        val planeRadius = 5f
-//        for (i in 0 until 10) {
-//            val objectModel = createObjectModel()
-//            val objectInstance = ModelInstance(objectModel)
-//
-//            val randomAngle = Random.nextDouble() * 2 * Math.PI
-//            val randomDistance = Random.nextDouble() * planeRadius
-//            val x = (randomDistance * Math.cos(randomAngle)).toFloat()
-//            val y = (randomDistance * Math.sin(randomAngle)).toFloat()
-//            val z = 0f
-//
-//            objectInstance.transform.setTranslation(x, y, z)
-//            objectInstance.transform.mul(randomRotation)
-//            objectInstances.add(objectInstance)
-//        }
-//    }
 
     override fun dispose() {
         modelBatch.dispose()
@@ -139,156 +106,19 @@ class OrbitVisualizerApp : ApplicationAdapter() {
         return modelBuilder.end()
     }
 
-
-
-
-
-
-// Mostly working plane
-//    private fun createPlaneModel(radius: Float, segments: Int): Model {
-//        val vertexSize = 3 + 3
-//        val numVertices = (segments + 1) * 2
-//        val indices = ShortArray(segments * 3)
-//        val vertices = FloatArray(numVertices * vertexSize)
-//        val angle = 2 * Math.PI / segments
-//
-//        for (i in 0 until segments) {
-//            val x = radius * Math.cos(i * angle).toFloat()
-//            val z = radius * Math.sin(i * angle).toFloat()
-//
-//            val normal = Vector3(x, 0f, z).nor()
-//
-//            vertices[i * vertexSize * 2] = x
-//            vertices[i * vertexSize * 2 + 1] = 0f
-//            vertices[i * vertexSize * 2 + 2] = z
-//            vertices[i * vertexSize * 2 + 3] = normal.x
-//            vertices[i * vertexSize * 2 + 4] = normal.y
-//            vertices[i * vertexSize * 2 + 5] = normal.z
-//
-//            vertices[i * vertexSize * 2 + 6] = 0f
-//            vertices[i * vertexSize * 2 + 7] = 0f
-//            vertices[i * vertexSize * 2 + 8] = 0f
-//            vertices[i * vertexSize * 2 + 9] = normal.x
-//            vertices[i * vertexSize * 2 + 10] = normal.y
-//            vertices[i * vertexSize * 2 + 11] = normal.z
-//
-//            if (i < segments - 1) {
-//                indices[i * 3] = (i * 2).toShort()
-//                indices[i * 3 + 1] = (i * 2 + 1).toShort()
-//                indices[i * 3 + 2] = (i * 2 + 2).toShort()
-//            }
-//        }
-//
-//        val mesh = Mesh(true, numVertices, indices.size,
-//            VertexAttribute(Usage.Position, 3, "a_position"),
-//            VertexAttribute(Usage.Normal, 3, "a_normal")
-//        )
-//        mesh.setVertices(vertices)
-//        mesh.setIndices(indices)
-//        val material = Material()
-//        material.set(ColorAttribute.createDiffuse(Color.BLUE))
-//        material.set(IntAttribute(IntAttribute.CullFace, 0)) // Disable backface culling
-//
-//        val modelBuilder = ModelBuilder()
-//        modelBuilder.begin()
-//        modelBuilder.part("planePart", mesh, GL20.GL_TRIANGLES, material)
-//        return modelBuilder.end()
-//    }
-
-
-//
-//    private fun createPlaneModel(radius: Float, segments: Int): Model {
-//        val vertexSize = 3 + 3
-//        val numVertices = segments * 2
-//        val vertices = FloatArray(numVertices * vertexSize)
-//        val angle = 2 * Math.PI / segments
-//
-//        for (i in 0 until segments) {
-//            val x = radius * Math.cos(i * angle).toFloat()
-//            val z = radius * Math.sin(i * angle).toFloat()
-//
-//            val normal = Vector3(x, 0f, z).nor()
-//
-//            vertices[i * vertexSize * 2] = x
-//            vertices[i * vertexSize * 2 + 1] = 0f
-//            vertices[i * vertexSize * 2 + 2] = z
-//            vertices[i * vertexSize * 2 + 3] = normal.x
-//            vertices[i * vertexSize * 2 + 4] = normal.y
-//            vertices[i * vertexSize * 2 + 5] = normal.z
-//
-//            vertices[i * vertexSize * 2 + 6] = 0f
-//            vertices[i * vertexSize * 2 + 7] = 0f
-//            vertices[i * vertexSize * 2 + 8] = 0f
-//            vertices[i * vertexSize * 2 + 9] = normal.x
-//            vertices[i * vertexSize * 2 + 10] = normal.y
-//            vertices[i * vertexSize * 2 + 11] = normal.z
-//        }
-//
-//        val mesh = Mesh(true, numVertices, 0,
-//            VertexAttribute(Usage.Position, 3, "a_position"),
-//            VertexAttribute(Usage.Normal, 3, "a_normal")
-//        )
-//        mesh.setVertices(vertices)
-//
-//        val material = Material()
-//        material.set(ColorAttribute.createDiffuse(Color.BLUE))
-//
-//        val modelBuilder = ModelBuilder()
-//        modelBuilder.begin()
-//        modelBuilder.part("planePart", mesh, GL20.GL_TRIANGLE_STRIP, material)
-//        return modelBuilder.end()
-//    }
-
-
-//
-//    private fun createPlaneModel(radius: Float, segments: Int): Model {
-//        val vertexSize = 3 + 3
-//        val numVertices = segments * 2
-//        val vertices = FloatArray(numVertices * vertexSize)
-//        val angle = 2 * Math.PI / segments
-//
-//        for (i in 0 until segments) {
-//            val x = radius * Math.cos(i * angle).toFloat()
-//            val z = radius * Math.sin(i * angle).toFloat()
-//
-//            val normal = Vector3(x, 0f, z).nor()
-//
-//            vertices[i * vertexSize * 2] = x
-//            vertices[i * vertexSize * 2 + 1] = 0f
-//            vertices[i * vertexSize * 2 + 2] = z
-//            vertices[i * vertexSize * 2 + 3] = normal.x
-//            vertices[i * vertexSize * 2 + 4] = normal.y
-//            vertices[i * vertexSize * 2 + 5] = normal.z
-//
-//            vertices[i * vertexSize * 2 + 6] = 0f
-//            vertices[i * vertexSize * 2 + 7] = 0f
-//            vertices[i * vertexSize * 2 + 8] = 0f
-//            vertices[i * vertexSize * 2 + 9] = normal.x
-//            vertices[i * vertexSize * 2 + 10] = normal.y
-//            vertices[i * vertexSize * 2 + 11] = normal.z
-//        }
-//
-//        val mesh = Mesh(true, numVertices, 0,
-//            VertexAttribute(Usage.Position, 3, "a_position"),
-//            VertexAttribute(Usage.Normal, 3, "a_normal")
-//        )
-//        mesh.setVertices(vertices)
-//
-//        val material = Material()
-//
-//        val modelBuilder = ModelBuilder()
-//        modelBuilder.begin()
-//        modelBuilder.part("planePart", mesh, GL20.GL_TRIANGLE_STRIP, material)
-//        return modelBuilder.end()
-//    }
-
     private fun createObjectModel(): Model {
         val modelBuilder = ModelBuilder()
-        modelBuilder.begin()
-        val attributes = (VertexAttributes.Usage.Position or VertexAttributes.Usage.Normal).toLong()
+        val radius = 0.5f
+        val divisions = 16
         val material = Material(ColorAttribute.createDiffuse(Color.GREEN))
-        val boxBuilder = modelBuilder.part("box", GL20.GL_TRIANGLES, attributes, material)
-        boxBuilder.box(0f, 0f, 0f, 0.25f, 0.25f, 0.25f)
+        val usageCode = Usage.Position.toLong() or Usage.Normal.toLong()
+
+        modelBuilder.begin()
+        modelBuilder.node().id = "sphere"
+        modelBuilder.part("sphere", GL20.GL_TRIANGLES, usageCode, material).apply {
+            SphereShapeBuilder.build(this, Matrix4(), radius, radius, radius, divisions, divisions)
+        }
+
         return modelBuilder.end()
     }
 }
